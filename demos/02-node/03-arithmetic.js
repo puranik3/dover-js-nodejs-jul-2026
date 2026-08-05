cabbd4a4a6a78ec046f5b3bb82f6aa4ce8715b98
Prashanth Puranik <puranik@digdeeper.in>
2026-08-05 09:57:30 +0530

    Module system

--- demos/02-node/03-arithmetic.js
+++ demos/02-node/03-arithmetic.js
@@ -1,8 +1,39 @@
-const add = ( x, y ) => x + y;
-const subtract = ( x, y ) => add( x, -y );
+console.log( 'executing arithmetic module' );
 
+// add() is "private" to the module
+const add = ( x, y ) => x + y;
+
+
+// default value assigned to module.exports -> {}
+// console.log( module.exports );  // {}
+
+// add to the default object
+// module.exports.subtract = ( x, y ) => add( x, -y );
+// module.exports.multiply = ( x, y ) => x * y;
+
+const subtract = ( x, y ) => add( x, -y );
 const multiply = ( x, y ) => x * y;
 
-module.exports = function() {
-    console.log( 'an function exported from arithmetic module' );
-};
+
+// module.exports = function() {
+//     console.log( 'an function exported from arithmetic module' );
+// };
+
+// module.exports = {
+//     // subtract: subtract,
+//     // multiply: multiply
+//     subtract,
+//     multiply
+// };
+
+/*export default */function introduce() {
+    console.log( 'i am arithmetic module' );
+}
+
+export {
+    introduce as default,
+    subtract,
+    multiply
+}
+
+console.log( 'end of arithmetic module' );
--- demos/02-node/03-use-arithmetic.js
+++ demos/02-node/03-use-arithmetic.js
@@ -1,4 +1,12 @@
 // relative paths for modules we create / absolute path for built-in and third-party modules
-const fn = require( './03-arithmetic' );
+// const arithmetic = require( './03-arithmetic' );
+// const arithmetic2 = require( './03-arithmetic' );
 
-fn();
+import introduce, { subtract, multiply } from './03-arithmetic.js';
+import introduce2, { subtract as subtract2 } from './03-arithmetic.js';
+
+// console.log( arithmetic );
+
+// console.log( arithmetic.subtract( 2, 3 ) );
+
+console.log( subtract( 2, 3 ) );
