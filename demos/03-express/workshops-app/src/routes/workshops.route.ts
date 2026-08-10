351f5a906d133a1963b7d737874995eadc232be6
Prashanth Puranik <puranik@digdeeper.in>
2026-08-10 10:05:16 +0530

    Step 8: Validating using Joi, Adding an error response, and sending structured responses

--- demos/03-express/workshops-app/src/routes/workshops.route.ts
+++ demos/03-express/workshops-app/src/routes/workshops.route.ts
@@ -1,6 +1,37 @@
 import express from 'express';
+import Joi from 'joi';
+
 // You can import JSON file!
 import workshops from '../data/workshops.json';
+
+// set up the Joi schema for validation
+const timeSchema = Joi.object({
+    hours: Joi.number().integer().min(0).max(23).required(),
+    minutes: Joi.number().integer().min(0).max(59).required(),
+});
+
+const workshopSchema = Joi.object({
+    name: Joi.string().required(),
+    category: Joi.string()
+        .valid('frontend', 'backend', 'database', 'devops', 'language', 'mobile')
+        .required(),
+    description: Joi.string().max(1024).required(),
+    startDate: Joi.string().isoDate().required(),
+    endDate: Joi.string().isoDate().required(),
+    startTime: timeSchema.required(),
+    endTime: timeSchema.required(),
+    speakers: Joi.array().items(Joi.string()).min(1).required(),
+    location: Joi.object({
+        address: Joi.string().required(),
+        city: Joi.string().required(),
+        state: Joi.string().required(),
+    }).required(),
+    modes: Joi.object({
+        inPerson: Joi.boolean().required(),
+        online: Joi.boolean().required(),
+    }).required(),
+    imageUrl: Joi.string().uri().required(),
+});
 
 let nextId = 13;
 
@@ -11,16 +42,43 @@ router
     .get((req, res) => {
         // status(), json(), redirect(), send(), sendFile() are added onto the Node JS response object
         // Content-Type HTTP header conveys the MIME type (format) of the data
-        res.json(workshops);
+        res.json({
+            status: 'success',
+            data: workshops,
+        });
     })
     .post((req, res) => {
         const newWorkshop = req.body;
 
-        newWorkshop.id = nextId;
-        ++nextId;
+        // Check if body is sent and not empty
+        if (!newWorkshop || Object.keys(newWorkshop).length === 0) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'The request body is empty. Workshop object expected.',
+            });
+        }
+
+        // Validate using Joi
+        const { error, value } = workshopSchema.validate(newWorkshop, {
+            abortEarly: false,
+            convert: false,
+        });
+
+        if (error) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Validation failed',
+                details: error.details.map((err) => err.message),
+            });
+        }
+
+        newWorkshop.id = nextId++;
         workshops.push(newWorkshop);
 
-        res.status(201).json(newWorkshop);
+        res.status(201).json({
+            status: 'success',
+            data: newWorkshop,
+        });
     });
 
 export default router;
